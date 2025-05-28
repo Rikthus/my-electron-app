24aa28b7d395b4f8a9dca9697be4b51f07702df4
Maximilien PEREIRA <maximilien.pereira@epsa.com>
2025-05-28 15:43:21 +0200

    fix: not working updater because missing repository config in package.json

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-require('update-electron-app')()
 const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('node:path')
+const { updateElectronApp } = require('update-electron-app')
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -10,19 +10,21 @@ const createWindow = () => {
       preload: path.join(__dirname, 'preload.js')
     }
   })
-
+  
   win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
   ipcMain.handle('ping', () => 'pong')
   createWindow()
-
+  
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+    })
 })
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
-})
+  })
+
+updateElectronApp()
